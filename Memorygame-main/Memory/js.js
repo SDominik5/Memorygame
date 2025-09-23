0902ab21d7102c29af14a4669ce67095ff5892e9
NemethBalazsHBSZ <nemeth.balazs@hbsz.edu.hu>
2025-09-23 10:38:51 +0200

    Add files via upload

---
+++ Memorygame-main/Memory/js.js
@@ -0,0 +1,7 @@
+function createCards(){
+    const main = document.getElementById('main')
+    for(let i = 0; i < 16; i++){
+        const div = document.createElement('div')
+        main.appendChild(div)
+    }
+}
