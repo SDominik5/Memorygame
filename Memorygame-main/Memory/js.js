217c54c029c4a20f3dbe0f1c934aacfa0b8b8305
Simon <79712743+SDominik5@users.noreply.github.com>
2025-09-23 13:00:20 +0200

    Delete Memorygame-main/Memory directory

--- Memorygame-main/Memory/js.js
+++
@@ -1,7 +0,0 @@
-function createCards(){
-    const main = document.getElementById('main')
-    for(let i = 0; i < 16; i++){
-        const div = document.createElement('div')
-        main.appendChild(div)
-    }
-}
